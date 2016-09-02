f5303acae2be50b7e006ec8ccf656be79fc685f9
Daniel Butler <cactusfly@gmail.com>
2016-09-02 19:01:22 +0000

    updated app.js and index.html

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,21 @@
-//create function
+var SayHello;
+
+//convert the SayHello string to lowercase and return it
+function ConvertHello(value) {
   //control and return statements
+  SayHello = value.toLowerCase();
+  return;  
+}
 
 //Prompt user for input and store variable
+SayHello = prompt("Say Hello!");
 
 //Call function
+ConvertHello(SayHello);
 
 //Alert user results
+if (SayHello === 'hello') {
+  alert("Hello World!");
+} else {
+  alert("Hello?");
+}
