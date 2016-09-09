ff4e1477a6002a54a1f3a0923a8bba7422ca9a86
Daniel Butler <cactusfly@gmail.com>
2016-09-09 16:30:55 -0400

    updating week 2 challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,21 +1,40 @@
-var SayHello;
+var helloPrompt;
+var helloResponses;
 
 //convert the SayHello string to lowercase and return it
 function ConvertHello(value) {
   //control and return statements
-  SayHello = value.toLowerCase();
-  return;  
+  helloPrompt = value.toLowerCase();
+  console.log("I converted " + helloPrompt);
+  return;
 }
 
 //Prompt user for input and store variable
-SayHello = prompt("Say Hello!");
+helloPrompt = prompt("Say Hello!");
 
-//Call function
-ConvertHello(SayHello);
+//set the array of helloResponses
+helloResponses = [
+  "No really, say Hello!",
+  "Why don't you say Hello?",
+  "But really why aren't you saying Hello?",
+  "PLEASE SAY HELLO",
+  "JUST SAY HELLO"
+]
+
 
 //Alert user results
-if (SayHello === 'hello') {
-  alert("Hello World!");
-} else {
-  alert("Hello?");
+for (i = 0; i < helloResponses.length; i++){
+
+  //Call function
+  ConvertHello(helloPrompt);
+  console.log("helloPrompt is " + helloPrompt)
+  if (helloPrompt === 'hello') {
+    alert("Hello World!");
+    console.log("helloPrompt is " + helloPrompt);
+    break;
+  } else {
+  // alert("Hello?");
+    helloPrompt = prompt(helloResponses[i]);
+    console.log("helloPrompt is " + helloPrompt);
+  }
 }
