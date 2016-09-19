9dd2391053ea10e600967a8588541561d43e50c9
Daniel Butler <cactusfly@gmail.com>
2016-09-19 00:57:13 -0400

    recommiting everything all over again

---
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -0,0 +1,23 @@
+var mainMenu = document.getElementById("menu"); //ul with id menu
+
+function openBlock (id) {
+    var block = document.getElementById(id);
+    var blocks = document.getElementById("main_content").getElementsByTagName("div");
+
+    for (var y = 0; y < blocks.length; y++) {
+        blocks[y].style.display = "none";
+    }
+    block.style.display = "block";
+
+    // TODO: remove the "active" class from all of the li elements inside the menu
+    $(this).siblings().removeClass("active");
+    // TODO: add the "active" class to the li element that contains the link that was clicked
+    $(this).addClass("active");
+}
+
+mainMenu.addEventListener("click", openBlock);
+
+// TODO: add the "hover" class to the menu items when you hover over them
+
+
+// TODO: set up the tooltip plugin on all of the links in the menu
