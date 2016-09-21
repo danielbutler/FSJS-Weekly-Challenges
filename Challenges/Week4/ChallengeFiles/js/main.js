b4b0ce54b3137a1b6987ad443ab55d2269bfd34a
Daniel Butler <cactusfly@gmail.com>
2016-09-20 22:19:12 -0400

    added page changination, hover, tooltips

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,22 +1,27 @@
-// var menuItem = $(".active");
-// var $menuItem = $("#menu a");
-// var $menuLi = $("#menu li");
-// var $subMenuItem;
+var $menuItem = $("#menu li");
+var $menuLinks = $("#menu a")
+// var testid = document.getElementById("plans");
+// var blocks = document.getElementById("main_content").getElementsByTagName("div");
+// for (var y = 0; y < blocks.length; y++) {
+//   blocks[y].style.display = "none";
+//   console.log(blocks[y]);
+// }
+// testid.style.display = "block";
+// console.log("what is testid? It is " + testid.id);
 
 function openBlock (id) {
+  console.log("This is id = " + id);
   var block = document.getElementById(id);
   var blocks = document.getElementById("main_content").getElementsByTagName("div");
-  // console.log(blocks);
   for (var y = 0; y < blocks.length; y++) {
     blocks[y].style.display = "none";
   }
   block.style.display = "block";
-  // plans.style.display = "block";
 
   // TODO: remove the "active" class from all of the li elements inside the menu
-  $("#menu li").siblings().removeClass("active")
+  $("#menu li").siblings().removeClass("active");
   // TODO: add the "active" class to the li element that contains the link that was clicked
-  $("#menu a").addClass("active");
+  //????????????????????????????????????????????????????????
 }
 
 //ok somehow this will get the other things to run but I don't know how to get the info "plans"?
@@ -25,20 +30,19 @@ function openBlock (id) {
 // openBlock("plans");
 
 $("#menu").on("click", "a", function(){
-  console.log($(this).attr("id"));
-  $subMenuItem = $(this).children().attr("id");
-  // openBlock($subMenuItem);
-  openBlock($subMenuItem);
+  $id = $(this).attr("id").slice(11);
+//   $subMenuItem = $(this).children().attr("id");
+//   // openBlock($subMenuItem);
+  openBlock($id);
 });
 
 
 // TODO: add the "hover" class to the menu items when you hover over them
-$("#menu a").hover(function(){
-  $(this).siblings().removeClass("hover");
-}, function(){
+$menuLinks.hover(function(){
+  $("#menu a").removeClass("hover");
   $(this).addClass("hover");
-  // console.log("hovering");
 });
+
 
 // TODO: set up the tooltip plugin on all of the links in the menu
 $('#menu a').tooltip({
