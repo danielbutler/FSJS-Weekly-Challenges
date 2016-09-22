134e62a938b06ccf3df796f8e444d1cd1df6ba58
danielbutler <cactusfly@gmail.com>
2016-09-22 19:31:41 -0400

    finally got the active class to be set on the active li

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,16 +1,5 @@
-var $menuItem = $("#menu li");
-var $menuLinks = $("#menu a")
-// var testid = document.getElementById("plans");
-// var blocks = document.getElementById("main_content").getElementsByTagName("div");
-// for (var y = 0; y < blocks.length; y++) {
-//   blocks[y].style.display = "none";
-//   console.log(blocks[y]);
-// }
-// testid.style.display = "block";
-// console.log("what is testid? It is " + testid.id);
-
 function openBlock (id) {
-  console.log("This is id = " + id);
+  id2 = "menu_items_" + id;
   var block = document.getElementById(id);
   var blocks = document.getElementById("main_content").getElementsByTagName("div");
   for (var y = 0; y < blocks.length; y++) {
@@ -19,27 +8,23 @@ function openBlock (id) {
   block.style.display = "block";
 
   // TODO: remove the "active" class from all of the li elements inside the menu
-  $("#menu li").siblings().removeClass("active");
+  $("#menu li").siblings().removeClass("active");$(this).addClass("active");
   // TODO: add the "active" class to the li element that contains the link that was clicked
-  // Still don't know how to do it?
-  // $("#menu a").parent().addClass("active");
+  activeLi = document.getElementById(id2);
+  $(activeLi).parent("li").addClass("active");
+
 }
 
-//ok somehow this will get the other things to run but I don't know how to get the info "plans"?
-//This was only working because menu items were "menu_item_plans"
-//This is incorrect. It somehow requires the above, but I don't see the connection
-// openBlock("plans");
-
-$("#menu").on("click", "a", function(){
+// On the li items in the menu id, when you click
+$("#menu li").on("click", "a", function(){
+  // it will grab the menu item and slice off "menu_items_"
   $id = $(this).attr("id").slice(11);
-//   $subMenuItem = $(this).children().attr("id");
-//   // openBlock($subMenuItem);
   openBlock($id);
 });
 
 
 // TODO: add the "hover" class to the menu items when you hover over them
-$menuLinks.hover(function(){
+$("#menu a").hover(function(){
   $("#menu a").removeClass("hover");
   $(this).addClass("hover");
 });
