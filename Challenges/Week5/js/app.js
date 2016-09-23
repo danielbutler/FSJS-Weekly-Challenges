860a47165a142ed762b38bb4b99963e43216f8d5
danielbutler <cactusfly@gmail.com>
2016-09-23 13:13:56 -0400

    updated app.js

--- Challenges/Week5/js/app.js
+++ Challenges/Week5/js/app.js
@@ -6,20 +6,27 @@ function addPokemon(name) {
     `).appendTo('#pokemon');
 };
 
-
 // 1.)  Use the PokéAPI from http://pokeapi.co along with jQuery's getJSON function to retrieve the first 20 Pokémon.
-// 1.1)  Use the addPokemon function to show each of the Pokémon names that were retrieved.  
+// 1.1)  Use the addPokemon function to show each of the Pokémon names that were retrieved.
 //Hint: Learn how to access resources via the documentation http://pokeapi.co/docsv2/#resource-lists
-
+{
+    "count": 365,
+    "next": "http://pokeapi.co/api/v2/evolution-chain/?limit=20&offset=20",
+    "previous": null,
+    "results": [{
+        "url": "http://pokeapi.co/api/v2/evolution-chain/1/"
+    }]
+}
 
 // 2.)  Use jQuery to create a click handler for the next and previous buttons.
 // 2.1) Use the "next" and "previous" properties of the pokemon resource object to get the next or previous list of Pokémon.
-// 2.2) When a user clicks next or previous, remove all existing Pokémon from the ul element and add the new list of Pokémon.  
-// 2.3) Use jQuery to disable the next/previous buttons if there are no more Pokémon to retrieve in that direction.  
+// 2.2) When a user clicks next or previous, remove all existing Pokémon from the ul element and add the new list of Pokémon.
+// 2.3) Use jQuery to disable the next/previous buttons if there are no more Pokémon to retrieve in that direction.
+
 
 /*  Super Awesome Bonus!
     When you click on a Pokémon name, hide all the names and show a larger card that contains details about that Pokémon such as their sprite (picture), name,
     type or anything else you would like to include.  Add a way to go back to the list when your're done looking at the detail.
 
-    Be creative, you can style/arrange the detail information however you like! 
+    Be creative, you can style/arrange the detail information however you like!
 */
