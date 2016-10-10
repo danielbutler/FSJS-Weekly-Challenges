9c04754fb35983d27ada1353b3ef81373bde8968
danielbutler <cactusfly@gmail.com>
2016-10-09 23:14:40 -0400

    updated app.js / github.js

--- Challenges/Week7/app.js
+++ Challenges/Week7/app.js
@@ -1,1 +1,6 @@
 //See readme for challenge instructions
+
+var github = require("./github.js");
+
+var users = process.argv.slice(2);
+users.forEach(github.getRepos);
--- Challenges/Week7/github.js
+++ Challenges/Week7/github.js
@@ -1,8 +1,11 @@
 //See readme for challenge instructions
 
+var https = require("https");
+var http = require('http');
+
 /*
  * Note about github api: requires User-Agent header to be set. This can be done
- * in Node by passing an options object (rather than a simple url string) as the 
+ * in Node by passing an options object (rather than a simple url string) as the
  * first param to the https.get() function
  *
  * So something like
@@ -16,6 +19,52 @@
  *
  */
 
+// Print out message
+ // function printMessage(username, data1, data2) {
+ //   message = username + " and " + data1 + " and " + data2;
+ //   console.log(message);
+ // }
+
+ // Print out error messages
+ function printError(error) {
+   console.error(error.message);
+ }
+
 function getRepos(username){
-  console.log('Repos for ' + username);
+  var options = {
+    hostname: "api.github.com",
+    path: "/users/:" + username + "/repos",
+    headers: {
+      'User-Agent': username
+    }
+  };
+  var request = https.get(options, function(response) {
+    console.log(options.path);
+    var body = "";
+    // Read the data
+    response.on('data', (chunk) => {
+      body += chunk;
+    });
+    response.on('end', function() {
+      if(response.statusCode === 200) {
+        try {
+          // Parse the data
+          var profile = JSON.parse(body);
+          var str = JSON.stringify(profile);
+          // Print the data
+          printMessage(username, profile.id, options.path);
+          // console.log(profile.id);
+        } catch(error) {
+        // Parse Error
+          printError(error);
+        }
+      } else {
+        // Status Code Error
+        printError({message: "There was an error getting the repos for " + username +". (" + http.STATUS_CODES[response.statusCode] + ")"});
+      }
+    });
+    console.log('Repos for ' + username);
+  });
 }
+
+module.exports.getRepos = getRepos;
